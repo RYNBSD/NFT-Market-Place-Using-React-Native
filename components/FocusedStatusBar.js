12aa3188bdcaac30e039ff8484058c7e4a2bf755
RYNBSD <112697887+RYNBSD@users.noreply.github.com>
2022-10-24 23:27:04 +0100

    Add files via upload

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'react-native'
+
+import { useIsFocused } from '@react-navigation/native'
+
+import React from 'react'
+
+const FocusedStatusBar = (props) => {
+  
+    const isFocused = useIsFocused();
+  
+    return (
+      isFocused ? <StatusBar animated {...props}/> : null
+  )
+}
+
+export default FocusedStatusBar
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBids from "./DetailsBids";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard,
+    CircleButton,
+    RectButton,
+    DetailsBids,
+    DetailsDesc,
+    SubInfo
+}
